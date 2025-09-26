5ea23395608a41e0860f6fb4f22a73e245d7bfec
Jawad Alarman <111539474+jawad0110@users.noreply.github.com>
2025-09-26 15:06:45 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,37 +1,39 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
 
-
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    JWT_SECRET: str
-    JWT_ALGORITHM: str
-
-    REDIS_URL: str
-    REDIS_HOST: str
-    REDIS_PORT: int
-
-    MAIL_USERNAME: str
-    MAIL_PASSWORD: str
-    MAIL_FROM: str
-    MAIL_PORT: int
-    MAIL_SERVER: str
-    MAIL_FROM_NAME: str
-    MAIL_STARTTLS: bool = True
-    MAIL_SSL_TLS: bool = False
-    USE_CREDENTIALS: bool = True
-    VALIDATE_CERTS: bool = True
-
+    DATABASE_URL : str
+    JWT_SECRET : str
+    JWT_ALGORITHM : str
+    REDIS_URL : str = "redis://:taqa0778@taqastore-sjz77-redis-master.taqastore-sjz77.svc.cluster.local:6379/0"
+    REDIS_HOST : str
+    REDIS_PORT :str
+    
+    MAIL_USERNAME : str
+    MAIL_PASSWORD : str
+    MAIL_FROM : str
+    MAIL_PORT : int
+    MAIL_SERVER : str
+    MAIL_FROM_NAME : str
+    MAIL_STARTTLS : bool = True
+    MAIL_SSL_TLS : bool = False
+    USE_CREDENTIALS : bool = True
+    VALIDATE_CERTS : bool = True
     DOMAIN: Optional[str] = None
-    FRONTEND_URL: str
-    ACCESS_TOKEN_EXPIRY_DAYS: int = 7
-    REFRESH_TOKEN_EXPIRY_DAYS: int = 30
-
+    FRONTEND_URL : str
+    ACCESS_TOKEN_EXPIRY_DAYS : int = 7
+    REFRESH_TOKEN_EXPIRY_DAYS : int = 30
+    
     STATIC_URL: str = "/static"
 
     model_config = SettingsConfigDict(
-        env_file=".env",
-        extra="ignore"
+        env_file = ".env",
+        extra = "ignore"
     )
 
+
 Config = Settings()
+
+broker_url = Config.REDIS_URL
+result_backend = Config.REDIS_URL
+broker_connection_retry_on_startup = True
